2eed103d63b9cd2eb9aac39f289ad818d9255493
junsu <sunsuking@gmail.com>
2022-09-01 17:15:32 +0900

    Initial project

---
+++ android/app/src/main/kotlin/com/themealways/brandu/brandu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.themealways.brandu.brandu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:brandu/screens/home/main.dart';
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: HomePage(),
+    );
+  }
+}
